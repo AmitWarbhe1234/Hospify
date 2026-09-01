614fd59e877729802184990bb1ecee181469a7d3
Amit Warbhe <amitwarbhe@gmail.com>
2026-09-01 10:00:49 +0530

    updated login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -27,7 +27,7 @@ function App() {
 
       <Routes>
 
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Login />} />
         <Route path="/activate" element={<ActivateAccount />} />
 
         <Route
